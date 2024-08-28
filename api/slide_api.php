50ec0d24354e0f3e89729e917ad708acf1722d61
PNCH <pnch.nax@gmail.com>
2024-08-28 13:49:15 +0700

    first commit

---
+++ api/slide_api.php
@@ -0,0 +1,31 @@
+{
+    "result": true,
+    "data": {
+        "total": 3,
+        "info": [
+            {
+                "id": 112,
+                "sequent": 1,
+                "filename": null,
+                "link": "55fgfdgdgdfg",
+                "dateAdd": "2024-08-21 14:34:31"
+            },
+            {
+                "id": 114,
+                "sequent": 2,
+                "filename": "1724225851_114_sld.jpg",
+                "link": null,
+                "dateAdd": "2024-08-21 14:37:31"
+            },
+            {
+                "id": 113,
+                "sequent": 3,
+                "filename": "1724225801_113_sld.jpg",
+                "link": null,
+                "dateAdd": "2024-08-21 14:36:41"
+            }
+        ]
+    },
+    "csrf": "4ea0101bec1f804e906168f4a727a34abe0981d0551c427aab791de11d1ccfca",
+    "lastActivity": 1724826488
+}
