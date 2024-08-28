4b48c3029ef00c4905abd416413ade12d1f308fd
PNCH <131460426+PN23CH@users.noreply.github.com>
2024-08-28 21:05:43 +0700

    structure-create

--- src/manage.php
+++ src/manage.php
@@ -1,11 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <title>News - slide - manage</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
+<style>
+    body {
+        font-family: "Sarabun", sans-serif;
+    }
+</style>
+
 <body>
-    
+    <div class="w-full box-border mx-auto">
+        <div class="bg-sky-200 h-16">NAVBAR</div>
+        <div class="flex justify-between items-center w-full">
+            <div class="flex min-w-[200px] w-[350px] max-w-[400px] h-screen bg-pink-200">SIDEMENU</div>
+            <div class="flex flex-col w-full h-screen px-10 py-8 gap-y-5">
+                <div class="text-2xl font-semiblod">ระบบ จัดการภาพสไลด์</div>
+                <div class="flex justify-end items-center bg-orange-300 gap-x-5">
+                    <div>REFRESH</div>
+                    <div>SAVE</div>
+                    <div>SELECT ALL</div>
+                    <div>DELETE</div>
+                </div>
+                <div class="yellow-slate-300">TABLE</div>
+                <div class="flex justify-between items-center  bg-slate-300">
+                    <div>ADD</div>
+                    <div>PAGINATION</div>
+                </div>
+            </div>
+        </div>
+        <div class="bg-rose-200">FOOTER</div>
+    </div>
 </body>
+
 </html>
