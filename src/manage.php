31d784e26feda60d220edbc4a568e21c8d2c7eec
KhunNet <131509141+KhunNet555@users.noreply.github.com>
2024-08-31 00:27:06 +0700

    rowcell

--- src/manage.php
+++ src/manage.php
@@ -29,7 +29,7 @@
                     <div>SELECT ALL</div>
                     <div>DELETE</div>
                 </div>
-                <table class="w-full">
+                <table data-practice="practice" class="w-full">
                     <thead>
                         <tr>
                             <th>ID</th>
@@ -52,52 +52,67 @@
 </body>
 
 <script>
-    const dataSlide = {
-        "result": true,
-        "data": {
-            "total": 3,
-            "info": [{
-                    "id": 112,
-                    "sequent": 1,
-                    "filename": null,
-                    "link": "55fgfdgdgdfg",
-                    "dateAdd": "2024-08-21 14:34:31"
-                },
-                {
-                    "id": 114,
-                    "sequent": 2,
-                    "filename": "1724225851_114_sld.jpg",
-                    "link": null,
-                    "dateAdd": "2024-08-21 14:37:31"
-                },
-                {
-                    "id": 113,
-                    "sequent": 3,
-                    "filename": "1724225801_113_sld.jpg",
-                    "link": null,
-                    "dateAdd": "2024-08-21 14:36:41"
-                }
-            ]
-        },
-        "csrf": "4ea0101bec1f804e906168f4a727a34abe0981d0551c427aab791de11d1ccfca",
-        "lastActivity": 1724826488
-    }
+    function trCell() {
+        const table = document.querySelector('[data-practice="practice"]');
+        const tableBody = table.querySelector(`tbody`);
+        const rowCell = document.getElementById('dataSlide');
 
-    function displaySlide() {
-        const tbodyslide = document.getElementById('dataSlide');
-        const dataSlideJson = dataSlide.data.info;
+        for (let i = 0; i < 2; i++) {
+            const row = document.createElement("tr");
+            const tdCell = document.createElement("td");
+            row.innerHTML = `<div>test test</div>`
+            row.appendChild(row);
+        }
+    };
+    trCell();
 
-        dataSlideJson.forEach(item => {
-            const itemSlide = document.createElement('div');
-            itemSlide.innerHTML = `
-                    <p>Filename: ${item.filename ? item.filename : 'No Filename'}</p>
-                    <p>Link: ${item.link ? item.link : 'No Link'}</p>
-                    <p>Date Added: ${item.dateAdd}</p>
-                    <hr>`;
-            tbodyslide.appendChild(itemSlide);
-        })
-    }
-    displaySlide();
+
+    // const dataSlide = {
+    //     "result": true,
+    //     "data": {
+    //         "total": 3,
+    //         "info": [{
+    //                 "id": 112,
+    //                 "sequent": 1,
+    //                 "filename": null,
+    //                 "link": "55fgfdgdgdfg",
+    //                 "dateAdd": "2024-08-21 14:34:31"
+    //             },
+    //             {
+    //                 "id": 114,
+    //                 "sequent": 2,
+    //                 "filename": "1724225851_114_sld.jpg",
+    //                 "link": null,
+    //                 "dateAdd": "2024-08-21 14:37:31"
+    //             },
+    //             {
+    //                 "id": 113,
+    //                 "sequent": 3,
+    //                 "filename": "1724225801_113_sld.jpg",
+    //                 "link": null,
+    //                 "dateAdd": "2024-08-21 14:36:41"
+    //             }
+    //         ]
+    //     },
+    //     "csrf": "4ea0101bec1f804e906168f4a727a34abe0981d0551c427aab791de11d1ccfca",
+    //     "lastActivity": 1724826488
+    // }
+
+    // function displaySlide() {
+    //     const tbodyslide = document.getElementById('dataSlide');
+    //     const dataSlideJson = dataSlide.data.info;
+
+    //     dataSlideJson.forEach(item => {
+    //         const itemSlide = document.createElement('div');
+    //         itemSlide.innerHTML = `
+    //                 <p>Filename: ${item.filename ? item.filename : 'No Filename'}</p>
+    //                 <p>Link: ${item.link ? item.link : 'No Link'}</p>
+    //                 <p>Date Added: ${item.dateAdd}</p>
+    //                 <hr>`;
+    //         tbodyslide.appendChild(itemSlide);
+    //     })
+    // }
+    // displaySlide();
 </script>
 
 </html>
