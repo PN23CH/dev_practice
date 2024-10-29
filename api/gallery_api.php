6adc572e721083018b0fcdef26fa80c95302400d
PNCH <pnch.nax@gmail.com>
2024-10-29 13:11:28 +0700

    api-for-submit-gal

---
+++ api/gallery_api.php
@@ -0,0 +1,28 @@
+{
+"result": true,
+"data": {
+"total": 30,
+"info": [
+{
+"id": 221,
+"sequent": 1,
+"filename": "37_201212201056321.jpg",
+"filepath": "dnm_file/slide/37_201212201056321.jpg",
+"link": "55fgfdgdgdfg",
+"filesize": "196",
+"category": "slide"
+},
+{
+"id": 222,
+"sequent": 2,
+"filename": "37_201212201056321.jpg",
+"filepath": "dnm_file/slide/37_201212201056321.jpg",
+"link": "55fgfdgdgdfg",
+"filesize": "196",
+"category": "slide"
+}
+]
+},
+"csrf": "4ea0101bec1f804e906168f4a727a34abe0981d0551c427aab791de11d1ccfca",
+"lastActivity": 1724826488
+}
